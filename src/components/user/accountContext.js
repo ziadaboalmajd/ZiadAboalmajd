54dc262322f9859796bc3508f4c60236b41817f7
Ziad Aboalmjd <ziadgamalaboalmajd@gmail.com>
2023-04-24 17:35:42 +0200

    main

--- src/components/user/accountContext.js
+++ src/components/user/accountContext.js
@@ -7,16 +7,16 @@ export const AccountContext = createContext();
 const Url = process.env.REACT_APP_DOMAIN_LINK + process.env.REACT_APP_LOGIN;
 
 const UserContext = ({ children }) => {
-  axios.defaults.withCredentials = true;
   const [user, setUser] = useState({ login: null });
   const [viewPro, setViewPro] = useState(null);
   const [rstCode, setRstCode] = useState(0);
   const [codePass, setCodePass] = useState(null);
-
+  
   const navigate = useNavigate();
-
+  
   const getCookies = async () => {
     try {
+      axios.defaults.withCredentials = true;
       axios.get(Url, { withCredentials: true }).then((response) => {
         if (response.data.login) {
           setUser({ login: true, user: response.data.user });

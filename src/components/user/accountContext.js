8300b11cf3a002b545c816798b0457907835d727
Ziad Aboalmjd <ziadgamalaboalmajd@gmail.com>
2023-04-24 17:28:38 +0200

    main

--- src/components/user/accountContext.js
+++ src/components/user/accountContext.js
@@ -7,6 +7,7 @@ export const AccountContext = createContext();
 const Url = process.env.REACT_APP_DOMAIN_LINK + process.env.REACT_APP_LOGIN;
 
 const UserContext = ({ children }) => {
+  axios.defaults.withCredentials = true;
   const [user, setUser] = useState({ login: null });
   const [viewPro, setViewPro] = useState(null);
   const [rstCode, setRstCode] = useState(0);
@@ -30,13 +31,17 @@ const UserContext = ({ children }) => {
 
   const test = async () => {
     try {
-      axios.get(process.env.REACT_APP_DOMAIN_LINK + "/test", { withCredentials: true }).then((response) => {
-        if (response.data) {
-          console.log(response.data);
-        } else {
-          console.log("error ya ziad");
-        }
-      });
+      axios
+        .get(process.env.REACT_APP_DOMAIN_LINK + "/test", {
+          withCredentials: true,
+        })
+        .then((response) => {
+          if (response.data) {
+            console.log(response.data);
+          } else {
+            console.log("error ya ziad");
+          }
+        });
     } catch (err) {
       console.log(err);
     }
